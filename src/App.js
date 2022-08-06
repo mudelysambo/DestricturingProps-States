9d6d2aaaee7b4736899632a0dd1eba248378ffd0
StudentLabA.S.O.M.C <91378831+mudelysambo@users.noreply.github.com>
2022-08-06 15:50:01 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Greet from "./greet";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Greet name="mudely" heroName="superman" />
+    </div>
+  );
+}
